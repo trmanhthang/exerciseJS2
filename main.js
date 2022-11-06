bf387e938d469b523d69dff57a902d2fde90a3a5
trmanhthang <bigzeen861998@gmail.com>
2022-11-06 21:45:01 +0700

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+var inputWidth = prompt('Nhập chiều dài');
+var inputHeight = prompt('Nhập chiều cao');
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+
+document.write('Diện tích là: ' + area);
+
